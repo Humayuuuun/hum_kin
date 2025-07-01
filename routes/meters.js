59d5d99d011a9fd3b8dfa7bf498d7e9e5db940de
KinsDT <Kinshuk.Goswami@kimbal.io>
2025-07-01 14:03:54 +0530

    post me

--- routes/meters.js
+++ routes/meters.js
@@ -1,7 +1,18 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 const { getMeters } = require('../controllers/metersController');
 
 router.get('/', getMeters);
+
+module.exports = router;
+*/
+
+const express = require('express');
+const router = express.Router();
+const { getMeters } = require('../controllers/metersController');
+
+// Add support for POST
+router.get('/', getMeters);
+router.post('/', getMeters);
 
 module.exports = router;
