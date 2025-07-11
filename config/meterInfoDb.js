7b84e82da26b63d088b274aa6a43d475b23a8c8c
KinsDT <Kinshuk.Goswami@kimbal.io>
2025-07-11 10:26:46 +0530

    munir

--- config/meterInfoDb.js
+++ config/meterInfoDb.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const meterInfoPool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'ct_ls',
-  password: 'ItsMe@03',
+  database: 'meter_info',
+  password: 'Kinshuk2214',
   port: 5432,
 });
 
